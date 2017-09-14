544a9cced61f2dca29301e70d9d31f4e9de04aa7
bh <psaung299@gmail.com>
2017-09-14 22:01:47 +0630

    Create table layout in application log

--- src/js/containers/Dashboard.js
+++ src/js/containers/Dashboard.js
@@ -14,6 +14,32 @@ class Dashboard extends Component {
         <Header />
         <div className="panel">
           <h3 className="panel__heading">Application Log</h3>
+          <tabel className="tabel tabel--border">
+            <tr className="heading">
+              <th>#</th>
+              <th>Date</th>
+              <th>Department</th>
+              <th>Notification</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2017/08/06</td>
+              <td>Department</td>
+              <td>Notification</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>2017/08/07</td>
+              <td>Department</td>
+              <td>Notification</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>2017/08/06</td>
+              <td>Department</td>
+              <td>Notification</td>
+            </tr>
+          </tabel>
         </div>
       </div>
     )
